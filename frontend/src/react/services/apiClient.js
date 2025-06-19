9a845f06f391bfd0f4f5048a367e54c27c6955d1
Stealr <baranovm003@yandex.ru>
2025-06-19 14:41:26 +0300

    [api] добавлены endpoints для auth

--- frontend/src/react/services/apiClient.js
+++ frontend/src/react/services/apiClient.js
@@ -1,24 +1,24 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  headers: {
-    'Content-Type': 'application/json',
-  },
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
 
 // Это прикрипление токена
-// apiClient.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem('authToken');
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
+apiClient.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('authToken');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 // тут обработка общих ошибок
 
