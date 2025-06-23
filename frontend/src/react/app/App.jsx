17bb40bc4017bed6715ab120940f4989fdaf6d63
Stealr <baranovm003@yandex.ru>
2025-06-23 23:18:13 +0300

    [fix] разборка последствий rebase

--- frontend/src/react/app/App.jsx
+++ frontend/src/react/app/App.jsx
@@ -1,12 +1,10 @@
 import { RouterProvider } from 'react-router-dom';
 import Provider from './Provider.jsx';
 import { router } from './router.jsx';
-import SideBar from '@components/ui/SideBar/SideBar';
 
 function App() {
   return (
     <Provider>
-      <SideBar />
       <RouterProvider router={router} />
     </Provider>
   );
--- frontend/src/react/components/containers/Layout/Layout.jsx
+++ frontend/src/react/components/containers/Layout/Layout.jsx
@@ -1,20 +1,25 @@
 import { Outlet } from 'react-router-dom';
 
 // import Header from '@components/ui/Header/Header';
-// import AsideBar from '@components/ui/AsideBar/AsideBar';
+import SideBar from '@components/ui/SideBar/SideBar';
 // import ScrollToTop from '@helpers/ScrollToTop/ScrollToTop.jsx';
 
 const Layout = () => {
-  return (
-    <>
-      {/* <Header /> */}
-      {/* <ScrollToTop /> */}
-      <main className='main'>
-        <Outlet /> 
-      </main>
-      {/* <Footer /> */}
-    </>
-  );
+    return (
+        <>
+            {/* <Header /> */}
+            {/* <ScrollToTop /> */}
+            {/* это заменить потом */}
+            <div className="Header" style={{ gridArea: 'header' }}>
+                header
+            </div>
+            <SideBar />
+            <main className="content">
+                <Outlet />
+            </main>
+            {/* <Footer /> */}
+        </>
+    );
 };
 
 export default Layout;
