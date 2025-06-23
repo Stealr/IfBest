de2a0cf5bfa6de1a4cda972a5afff9975b7e0b71
NordBeard <124629189+NordBeard@users.noreply.github.com>
2025-06-24 00:52:54 +0300

    Add files via upload

--- frontend/src/react/components/containers/Layout/Layout.jsx
+++ frontend/src/react/components/containers/Layout/Layout.jsx
@@ -1,21 +1,15 @@
 import { Outlet } from 'react-router-dom';
 
-// import Header from '@components/ui/Header/Header';
+import Header from '@components/ui/Header/Header';
 // import AsideBar from '@components/ui/AsideBar/AsideBar';
 // import ScrollToTop from '@helpers/ScrollToTop/ScrollToTop.jsx';
 
 const Layout = () => {
   return (
     <>
-      {/* <ScrollToTop /> */}
-      {/* <Header /> */}
-      {/* <AsideBar /> */}
-      
-      {/* это заменить потом */}
-      <div className='Header' style={{gridArea: 'header'}}>header</div>
-      <div className='sidebar' style={{gridArea: 'sidebar'}}>sidebar</div>
-      <main className='content'>
-        <Outlet /> 
+      <Header />
+      <main>
+        <Outlet />
       </main>
     </>
   );
