da01e114d2cbdf6947e465e552fa6172b826af53
Stealr <baranovm003@yandex.ru>
2025-06-26 15:12:12 +0300

    переместил иконки sidebar в подпапку sidebar

--- frontend/src/react/components/ui/SideBar/sidebarData.jsx
+++ frontend/src/react/components/ui/SideBar/sidebarData.jsx
@@ -1,8 +1,8 @@
-import MenuIcon from '@assets/svg/menu.svg?react';
-import HomeIcon from '@assets/svg/home.svg?react';
-import PlayIcon from '@assets/svg/play.svg?react';
-import ListIcon from '@assets/svg/list.svg?react';
-import MusicIcon from '@assets/svg/music.svg?react';
+import MenuIcon from '@assets/svg/sidebar/menu.svg?react';
+import HomeIcon from '@assets/svg/sidebar/home.svg?react';
+import PlayIcon from '@assets/svg/sidebar/play.svg?react';
+import ListIcon from '@assets/svg/sidebar/list.svg?react';
+import MusicIcon from '@assets/svg/sidebar/music.svg?react';
 
 export const sidebarIcons = [
   { component: <MenuIcon width="24" height="24" />, label: 'Menu' },
