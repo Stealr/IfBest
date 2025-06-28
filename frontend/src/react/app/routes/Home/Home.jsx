1ede84538927ad6d2af64ecc0be91da9c22b7f3a
Stealr <baranovm003@yandex.ru>
2025-06-28 17:28:24 +0300

    [feat] реализована обертка для секций

--- frontend/src/react/app/routes/Home/Home.jsx
+++ frontend/src/react/app/routes/Home/Home.jsx
@@ -2,6 +2,7 @@ import { CardVideo } from '@components/cards/CardVideo/CardVideo.jsx';
 import MainGrid from '@components/contentLayouts/MainGrid/MainGrid';
 import CardList from '@components/contentLayouts/CardList/CardList';
 import ArticleGrid from '@components/contentLayouts/ArticleGrid/ArticleGrid';
+import ContentSection from '@components/containers/ContentSection/ContentSection';
 import './home.scss';
 
 function Home() {
@@ -17,20 +18,34 @@ function Home() {
 
     return (
         <div className="homepage">
-            <MainGrid>
-                {Array.from({ length: 6 }, (_, index) => (
-                    <CardVideo key={index} />
-                ))}
-            </MainGrid>
-            <CardList typeCard={'video'} />
-            <MainGrid>
-                {Array.from({ length: 6 }, (_, index) => (
-                    <CardVideo key={index} />
-                ))}
-            </MainGrid>
-            <ArticleGrid />
-            <CardList typeCard={'audio'} />
-            
+            <ContentSection title={'Популярное'} href={'test'}>
+                <MainGrid>
+                    {Array.from({ length: 6 }, (_, index) => (
+                        <CardVideo key={index} />
+                    ))}
+                </MainGrid>
+            </ContentSection>
+
+            <ContentSection title={'Короткие видео'} href={'test'}>
+                <CardList typeCard={'video'} />
+            </ContentSection>
+
+            <ContentSection>
+                <MainGrid>
+                    {Array.from({ length: 6 }, (_, index) => (
+                        <CardVideo key={index} />
+                    ))}
+                </MainGrid>
+            </ContentSection>
+
+            <ContentSection>
+                <ArticleGrid />
+            </ContentSection>
+
+            <ContentSection title={'Аудио'} href={'test'}>
+                <CardList typeCard={'audio'} />
+            </ContentSection>
+
             {/* <h1 className="homepage__header title1-medium">title 1</h1>
             <h2 className='title2-medium'>title 2</h2>
             <p>testing</p>
---
+++ frontend/src/react/components/containers/ContentSection/ContentSection.jsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router';
+import './content_section.scss';
+
+/**
+ * Description
+ * Если сеции не нужен header, то не указывать title и href
+ * @param {string} title 'название секции'
+ * @param {string} href 'ссылка для перехода по кнопке смотреть больше'
+ * @param {any} children 'main content of section'
+ */
+function ContentSection({ title, href, children }) {
+    return (
+        <section className="section-content">
+            {title || href ? (
+                <div className="section-content__header">
+                    <p className="headline2">{title}</p>
+                    <Link to={href} className="section-content__more-btn">
+                        Смотреть больше
+                    </Link>
+                </div>
+            ) : null}
+            {children}
+        </section>
+    );
+}
+
+export default ContentSection;
